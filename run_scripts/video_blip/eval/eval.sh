7caa60f7791c05df19d59ae440fd7d1a571fe4da
xyh97 <xyhechoes@gmail.com>
2024-03-05 13:23:48 -0800

    delete

--- run_scripts/video_blip/eval/eval.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=0,7 python -m torch.distributed.run --nproc_per_node=2 --master_port=29552 evaluate.py --cfg-path lavis/projects/video_blip/eval/eval_aug.yaml
--- run_scripts/video_blip/eval/eval_para.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.run --nproc_per_node=4 --master_port=29552 evaluate.py --cfg-path lavis/projects/video_blip/eval/eval_para.yaml
